d3f1833652b5a4378bc2eb6026017aa91c7441fb
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-17 15:41:29 +0530

    ADDED:Program to add data at beginning

--- src/com/linklist/LinkList.java
+++ src/com/linklist/LinkList.java
@@ -1,15 +1,13 @@
 package com.linklist;
 
-public class LinkList {
+public class LinkList<T> {
      static Node head;
 
     public static void main(String[] args) {
         LinkList list = new LinkList();
-        list.head = new Node(57);
-        Node mySecondNode = new Node(30);
-        Node myThirdNode = new Node(70);
-        head.next=mySecondNode;
-        mySecondNode.next=myThirdNode;
+        list.insertFirst(70);
+        list.insertFirst(30);
+        list.insertFirst(56);
         print(list);
     }
     public static void print(LinkList list){
@@ -19,6 +17,13 @@ public class LinkList {
             System.out.print(currentNode.data+"->");
             currentNode=currentNode.next;
         }
+    }
 
+    public void insertFirst(T data){
+        Node <T> node = new Node<>();
+        node.data=data;
+        node.next=null;
+        node.next=head;
+        head=node;
     }
 }
